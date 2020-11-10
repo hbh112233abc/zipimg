a20d0cb66980ba7bdd1f069d6efef6552f8afe2d
hbh112233abc <hbh112233abc@163.com>
2020-11-10 18:53:29 +0800

    增加daemon模式

--- python/auto_zipimg.py
+++ python/auto_zipimg.py
@@ -8,6 +8,7 @@ import sys
 import os
 import getopt
 import time
+import daemon
 
 from watchdog.observers import Observer
 from watchdog.events import *
@@ -29,8 +30,8 @@ class AutoZipimg(FileSystemEventHandler):
             print("directory moved from {0} to {1}".format(
                 event.src_path, event.dest_path))
         else:
-            print("file moved from {0} to {1}".format(
-                event.src_path, event.dest_path))
+            print("file moved from {0} to {1}".format(event.src_path,
+                                                      event.dest_path))
 
     def on_created(self, event):
         if event.is_directory:
@@ -72,33 +73,42 @@ def prior_zipimg(watch_dir, app_path=''):
         ext = os.path.splitext(filepath)[-1]
         if ext not in ZipimgServer.ZIP_EXT:
             continue
-        bk_file = filepath+'.bk'
+        bk_file = filepath + '.bk'
         if os.path.isfile(bk_file):
             continue
         zipimg.zip(filepath)
 
 
-def check_params():
+def main():
     watch_dir = ''
     app_path = ''
     prior = False
+    is_daemon = False
     try:
         argv = sys.argv[1:]
-        opts, args = getopt.getopt(argv, "hpw:a:", ["watch_dir=", "app_path="])
+        opts, args = getopt.getopt(argv, "hpdw:a:",
+                                   ["watch_dir=", "app_path="])
     except getopt.GetoptError:
-        print('auto_zipimg.py -w <watch_dir> -a <app_path>')
+        print('auto_zipimg.py -w <watch_dir> -a <app_path> [-p] [-d]')
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print('auto_zipimg.py -w <watch_dir> -a <app_path> [-p]')
+        if opt in ('-h', '--help'):
+            print('auto_zipimg.py -w <watch_dir> -a <app_path> [-p] [-d]')
+            print('-w | --watch_dir <监控目录> [必须] 对该目录进行监控图片创建后进行压缩处理')
+            print('-a | --app_path <压缩软件路径> [选填] 压缩软件路径,win环境须传参')
+            print('-p | --prior [选填] 预处理已存在的文件')
+            print('-d | --daemon [选填] unix环境 程序后台运行')
             sys.exit()
         if opt in ("-w", "--watch_dir"):
             watch_dir = arg
         elif opt in ("-a", "--app_path"):
             app_path = arg
-        elif opt == '-p':
+        elif opt in ('-p', '--prior'):
             # 预处理已存在的文件
             prior = True
+        elif opt in ('-d', '--daemon'):
+            is_daemon = True
+
     if not watch_dir:
         print('watch_dir must')
         sys.exit(2)
@@ -109,11 +119,14 @@ def check_params():
     if prior:
         prior_zipimg(watch_dir, app_path)
 
-    return watch_dir, app_path
+    if is_daemon:
+        with daemon.DaemonContext():
+            watch(watch_dir, app_path)
+
+    watch(watch_dir, app_path)
 
 
-def main():
-    watch_dir, app_path = check_params()
+def watch(watch_dir, app_path):
     observer = Observer()
     event_handler = AutoZipimg(app_path=app_path)
     observer.schedule(event_handler, watch_dir, True)
